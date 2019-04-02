b698d4d1b3c4d4772e3d1e6476215c7453b36f5d
chrispetito <christopherpetito@gmail.com>
2019-04-01 18:27:04 -0700

    add methods using the object name and dot notation in objecs.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -31,9 +31,6 @@ const kennan = {
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
   "gender": "M",
-  speak: function() {
-    return 'Hello, my name is Kennan!';
-  }
 }
 
 const keven = {
@@ -55,9 +52,6 @@ const antonietta = {
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
   "gender": "F",
-  multiplyNums: function (num1, num2) {
-    return num1 * num2;
-  }
 }
 
 
@@ -86,9 +80,15 @@ console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
+kennan.speak = function () {
+  return `Hello, my name is ${this.name}!`;
+}
 console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
+antonietta.multiplyNums = function (num1, num2) {
+  return num1 * num2;
+}
 console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
